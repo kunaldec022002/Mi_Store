1a7d7f9a58504139182ba06e6a377efb2b918d08
kunaldec022002 <kunalingale72@gmail.com>
2023-12-11 16:10:43 +0530

    initial changes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Home from "./views/Home/Home"
 import Contact from "./views/Contact/Contact"
 import Laptop from "./views/Laptop/Laptop"
-import Login from "./views/Login/Login"
-import Signup from "./views/Signup/Signup"
+
 import Smartphone from "./views/Smartphone/Smartphone"
 import Tv from "./views/Tv/Tv"
 import Lifestyle from './views/Lifestyle/Lifestyle';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import ShowDetails from './views/ShowDetails/ShowDetails'
 
 
@@ -29,14 +27,9 @@ const router = createBrowserRouter([
       path: "/laptop",
       element: <Laptop />,
     },
-    {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
+   
+    
+  
       {
         path: "/smartphone",
         element: <Smartphone />,
